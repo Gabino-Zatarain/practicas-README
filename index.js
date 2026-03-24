639e9dc153709d0077294c4d9534bc9316c6976b
Gabino-Zatarain <gzatarainjimenez@gmail.com>
2026-03-24 13:32:25 -0700

    Implementación de endpoint GET /usuario

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+// Ruta principal
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando correctamente');
+});
+
+// NUEVA RUTA: Endpoint /usuario
+app.get('/usuario', (req, res) => {
+  // Objeto hardcodeado (puedes poner tus datos)
+  const usuario = {
+    id: 101,
+    nombre: 'Gabino Alonso',
+    rol: 'Desarrollador Backend'
+  };
+
+  // Enviamos el objeto como JSON
+  res.json(usuario);
 });
 
 app.listen(3000, () => {
