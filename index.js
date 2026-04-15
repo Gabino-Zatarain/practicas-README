df331b452e88a69c50fa407a6cce3beb7feb37e9
Gabino-Zatarain <gzatarainjimenez@gmail.com>
2026-04-14 18:56:38 -0700

    Integración exitosa de PostgreSQL

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require('express');
+const pool = require('./db'); // Importamos la conexión nueva
 const app = express();
 
-// Ruta principal
-app.get('/', (req, res) => {
-  res.send('API funcionando correctamente');
-});
-
-// NUEVA RUTA: Endpoint /usuario
+// --- Ruta de la actividad anterior ---
 app.get('/usuario', (req, res) => {
-  // Objeto hardcodeado (puedes poner tus datos)
   const usuario = {
     id: 101,
     nombre: 'Gabino Alonso',
     rol: 'Desarrollador Backend'
   };
-
-  // Enviamos el objeto como JSON
   res.json(usuario);
 });
+
+// --- Configuración de la nueva actividad ---
+app.get('/', (req, res) => {
+  res.send('API funcionando y conectada');
+});
+
+// Prueba de conexión a PostgreSQL
+pool.connect()
+  .then(() => {
+    console.log('Conexión exitosa a PostgreSQL');
+  })
+  .catch((err) => {
+    console.error('Error de conexión', err);
+  });
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
